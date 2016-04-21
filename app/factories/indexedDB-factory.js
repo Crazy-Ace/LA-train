149079288ca29fc6913be0989bda2964bdcd952f
Alan Pereira Mortoni <mortoni.alan@hotmail.com>
2016-04-21 11:46:19 +1000

    fix: indexedDB commit test for new branch

--- app/factories/indexedDB-factory.js
+++ app/factories/indexedDB-factory.js
@@ -4,7 +4,8 @@
   	//window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
   	//window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
   	//window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-
+    //teste para branch nova
+    
   	var defaultIDBSetting = {
   		name: "callTrain",
   		version: 1,
